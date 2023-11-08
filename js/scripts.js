00674628a5921d2a7036822c3456d5ff5a09b04e
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-11-08 08:08:58 +0300

    Question 2 done

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 
 ///////////////////Question 1/////////////
 
+document.getElementById("title").innerHTML =
+  "INSPECT To See the OUTPUT use F12 ";
 
 console.log("24>3 :", 24 > 3);
 console.log('2 < "12" :', 2 < "12");
@@ -21,6 +23,9 @@ console.log(
 );
 
 
+
+
+
 /** Js has 6 falsy values 
  * 0
  * false
@@ -29,3 +34,39 @@ console.log(
  * Null
  * NaN
  */
+
+
+
+// ////////////// Question 2 /////////////////////
+
+console.log(1==1) // True - the value are equal
+console.log(1===1) //Ture -both value and type are equal
+console.log('1'===1) //False - the same value but d/f data type
+
+console.log(x = (1 == true)); // True - The value of true is 1
+
+let x = 10;
+let y = (x > 5) && (x < 15)
+console.log(y); // true  - && expacts both expression should be true to retun true
+
+let num = 5;
+num += 3;
+console.log(num); //8 -num = num+3
+
+
+var num1 = 10;
+var num2 = num1++;
+console.log(num2); // 11 - it increased by 1
+
+
+let a = 1;
+let b = (a !== 2);
+console.log(b); // true - !false
+
+console.log(+"2" + 2); // 4 -js assume that it sould be add
+console.log("2" + 2); //22 -concat 
+console.log(+"a" + 2); // NaN - we can not add string a with number 2 mathematically 
+
+console.log(7 % 3); //1 - calculate the reminder
+
+console.log(2 + true); // 3 - true value is 1 so 2+1 == 3
