453cce2629e7270bdddeea47801358fd5d5f8ccc
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-11-18 11:27:42 +0300

    loop practice

--- loop.js
+++ loop.js
@@ -51,9 +51,22 @@
 // }while(i<10)
 
 
-for(let i=0;i<6;i++){
-    for(let j=1;j<i;j++){
-        document.write("*"+" ")
-    }
-    document.write("<br>")
+// for(let i=0;i<6;i++){
+//     for(let j=1;j<i;j++){
+//         document.write("*"+" ")
+//     }
+//     document.write("<br>")
+// }
+
+// for (let i = 0; i < 6; i++) {
+//   for (let j = 1; j < i; j++) {
+//     document.write( j+ " ");
+//   }
+//   document.write("<br>");
+// }
+for (let i = 1; i < 6; i++) {
+  for (let j = 1; j < i; j++) {
+    document.write(i + " ");
+  }
+  document.write("<br>");
 }
