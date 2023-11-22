9df98b24e4c9ec55c75cfbabd5d31132cb33c4f9
Getachew Muhabaw <getachewmuhabaw@gmail.com>
2023-11-22 12:02:46 +0300

    Factorial Question

--- js/Advanced js/advanced_algorithim.js
+++ js/Advanced js/advanced_algorithim.js
@@ -69,3 +69,71 @@ function reverseCompare(num) {
 
 // reverseCompare(72);// Ok
 // reverseCompare(23);//NOt Ok
+
+
+
+/**
+ * Question 3
+● Write a function that takes a positive integer and returns the factorial of the number.
+Notes: The factorial of 0 is 1. Ex: factorial seven is : 1 × 2 × 3 × 4 × 5 × 6 × 7. The
+factorial of any positive integer x is x * (x - 1) * (x - 2) * . . . . . . * 1 (ex: factorial of 4 is
+4 * 3 * 2 * 1 = 24)
+○ Test 1: returnFactorial(5) outputs 120
+○ Test 2: returnFactorial(6) outputs 720
+○ Test 3: returnFactorial(0) outputs 1
+ */
+
+function returnFactorial(num){
+    let f=1
+    if(Number(num)>=0 && Number.isInteger(num)){
+        if(num ===0 || num ===1){
+            return f
+        }
+        else {
+           for(let i=num; i>0; i--){
+             f*=i
+           } 
+           return f
+        }
+    }
+}
+let test1=returnFactorial(0)
+let test2 = returnFactorial(5);
+let test3 = returnFactorial(6)
+// console.log(test1)//1
+// console.log(test2)//120
+// console.log(test3)//720
+
+
+// Method 2 -------
+
+const factorial = (num) => {
+  if (num < 0) {
+    throw new Error("num must not be negative");
+  }
+  if (num ===0 || num ===1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+};
+
+// console.log(factorial(1))//1
+// console.log(factorial(5))//120
+// console.log(factorial(6))//720
+
+
+/**
+ * Question 4 (Meera array)
+A Meera array is defined to be an array containing only numbers as its elements and for
+all n values in the array, the value n*2 is not in the array. So [3, 5, -2] is a Meera array
+because 3*2, 5*2 or 2*2 are not in the array. But [8, 3, 4] is not a Meera array because
+2*4=8 and both 4 and 8 are elements found in the array. Write a function that takes an
+array of numbered elements and prints “I am a Meera array” in the console if its array
+does NOT contain n and also n*2 as value. Otherwise, the function prints “I am NOT a
+Meera array”
+○ Test 1: checkMeera([10, 4, 0, 5]) outputs “I am NOT a Meera array” because 5 *
+2 is 10
+○ Test 2: checkMeera([7, 4, 9]) outputs “I am a Meera array”
+○ Test 1: checkMeera([1, -6, 4, -3]) outputs “I am NOT a Meera array” because -3
+*2 is -6
+ */
